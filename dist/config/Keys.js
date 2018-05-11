0f34c6b1f5214eacb90c560ca02e8352672e1c21
djcoyer <dcoyer@lwolf.com>
2018-05-11 12:32:18 -0500

    Removed dist folder.

--- dist/config/Keys.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoURI = "mongodb://dcoyer:password@ds119490.mlab.com:19490/star-wars-dev";
-exports.mongoURI = mongoURI;
--- dist/constants/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TYPES = {
-    CharacterService: Symbol.for("CharacterService"),
-    CharacterRepository: Symbol.for("CharacterRepository")
-};
-exports.default = TYPES;
